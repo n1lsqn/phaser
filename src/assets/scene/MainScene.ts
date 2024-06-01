24261a33e737b1d45fe99d9899129c327fbdb3ca
n1lsqn <owner@n1l.dev>
2024-06-01 20:56:37 +0900

    feat: add DialogBox

--- src/assets/scene/MainScene.ts
+++ src/assets/scene/MainScene.ts
@@ -1,19 +1,45 @@
-export class MainScene extends Phaser.Scene {
-    constructor() {
-        super({ key: 'MainScene' });
-    }
-    create() {
-        const { width, height } = this.game.canvas;
-        
-        this.add.image(width / 2, height / 2, 'street');
-        this.add.text(width / 2, height / 2, 'クリックでエンディング', { fontFamily: 'arial', fontSize: '60px' }).setOrigin(0.5);
+import { DialogBox, DialogBoxConfig } from '../class/DialogBox';  // 追加
 
-        const zone = this.add.zone(width / 2, height / 2, width, height);
-        zone.setInteractive({
-            useHandCursor: true
-        });
-        zone.on('pointerdown', () => {
-            this.scene.start('EndingScene');
-        });
-    }
+export class MainScene extends Phaser.Scene {
+  constructor() {
+    super('main');
+  }
+
+  create() {
+    const { width, height } = this.game.canvas;
+
+    this.add.image(width/2, height/2, 'street');
+
+    const textStyle: Phaser.Types.GameObjects.Text.TextStyle = {
+      fontFamily: '"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif',
+      fontSize: '24px'
+    };
+
+    const dialogBoxHeight = 150;
+    const dialogBoxMargin = 10;
+    const dialogBoxConfig: DialogBoxConfig = {
+      x: width/2,
+      y: height - dialogBoxMargin - dialogBoxHeight/2,
+      width: width - dialogBoxMargin*2,
+      height: dialogBoxHeight,
+      padding: 10,
+      margin: dialogBoxMargin,
+      textStyle: textStyle
+    };
+
+    const dialogBox = new DialogBox(this, dialogBoxConfig);
+
+    dialogBox.setText('クリックでエンディングへ ▼');
+    dialogBox.setActorNameText('NAME');
+
+    this.add.existing(dialogBox);
+
+    const zone = this.add.zone(width/2, height/2, width, height);
+    zone.setInteractive({
+      useHandCursor: true
+    });
+    zone.on('pointerdown', () => {
+      this.scene.start('ending');  // EndingSceneに遷移
+    });
+  }
 }
